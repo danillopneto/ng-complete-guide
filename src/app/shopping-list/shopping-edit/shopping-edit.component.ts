c7de0ee45694e1b6d58d24e3e02c73a4e5164943
Danillo Pinheiro Neto <danillopneto@outlook.com>
2022-03-21 21:59:34 -0300

    Working with Services & Dependency Injection.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,9 +10,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false}) nameInput: ElementRef;
   @ViewChild('amountInput', { static: false}) amountInput: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -25,7 +25,7 @@ export class ShoppingEditComponent implements OnInit {
     }
 
     const ingredient = new Ingredient(ingredientName, ingredientAmount);
-    this.ingredientAdded.emit(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
     this.nameInput.nativeElement.value = '';
     this.amountInput.nativeElement.value = '';
   }
